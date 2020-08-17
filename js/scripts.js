ca2334142e5c5fef8b1c7050118d9dbc7e052c07
Kevinesolberg <kevinesolberg@gmail.com>
2020-08-17 12:21:44 -0500

    Add files via upload
    p-word

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,16 @@ function iframe_swap(hide, show){
 	console.log('test');
 
 }
+
+function onSubmit(form) {
+  
+  const password = form.token || form.querySelector('#password').value;
+
+  if (password == 'Fuck12'){
+  	$('#body_tag').css('display', 'block');
+  	$('#loginForm').css('display', 'none');
+  	console.log(password);
+  } else {
+  	window.location.reload();
+  }
+}
